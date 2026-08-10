010714a8422b22c1a7c13bb1eb15c1d5221920b4
Adryan <adryancorrea06@gmail.com>
2026-08-10 20:33:57 -0300

    Instanciando e declarando atributos e métodos.

---
+++ ClasseConta/Conta.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClasseConta
+{
+    public class Conta
+    {
+        // declaração de atributos
+        public int numero;
+        public string titular;
+        public double saldo;
+        // declaração dos métodos (funções)
+        public void Sacar()
+        {
+
+        }
+        public void Depositar()
+        {
+
+        }
+        
+    }
+}
---
+++ ClasseConta/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClasseConta;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Exemplos de instâncias...");
+        // declaração da variável c1, do tipo Conta
+        Conta c1;
+        // instancia de 1 objeto
+        // c1 esta recebendo uma alocação de memória com a instrução "new" e chamando o construtor da classe "Conta()"
+        c1 = new Conta();
+        // para utilizar o objeto:
+        c1.numero = 19;
+        c1.titular = "Ana";
+        c1.saldo = 109.10;
+
+        Console.WriteLine($"Número: {c1.numero} \tTitular: {c1.titular} \tSaldo: {c1.saldo:c}");
+        // exemplo com digitação
+        // instanciação do objeto c2
+        
+        Conta c2 = new Conta();
+    }
+}
